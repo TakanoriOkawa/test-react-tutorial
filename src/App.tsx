bdeb4ee7b9da337dfd509a4090601efc28809726
Taka <okawatakanori9916@gmail.com>
2022-12-21 00:00:01 +0900

    bootstrap jsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,21 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { Col, Row } from 'react-bootstrap';
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React hoge
-        </a>
-      </header>
+      <Row>
+        <Col>
+        <h1>テストチュートリアル</h1>
+        </Col>
+      </Row>
+
+      <Row>
+        <Col></Col>
+      </Row>
     </div>
   );
 }
