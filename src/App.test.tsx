77492d7273dd9cec77b9a361b2d19a74f919473b
Taka <okawatakanori9916@gmail.com>
2022-12-20 23:52:17 +0900

    fist testing

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -2,8 +2,10 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
+// testの内容を文字列で記述
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/learn react hoge/i);
+  // expect:期待する
   expect(linkElement).toBeInTheDocument();
 });
